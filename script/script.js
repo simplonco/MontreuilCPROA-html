82bc53cc0dabb68e2f156cd1b99520a1f27a5698
PaulPourtout <paulpourtout@gmail.com>
2017-09-19 16:53:09 +0200

    removed console logs

--- script/script.js
+++ script/script.js
@@ -5,10 +5,7 @@
 
     materialInputs.forEach((materialInput, index) => {
         materialInput.addEventListener('focus', () => {
-            console.log('index', index);
-
             if (materialInput.childNodes[1]) {
-                console.log('materialInput', materialInput.childNodes[1]);
                 materialInput.childNodes[1].style.color = "#009688";
                 materialInput.childNodes[1].style.fontSize = "0.8em";
                 materialInput.childNodes[1].style.top = "0";
@@ -17,10 +14,7 @@
         }, true);
 
         materialInput.addEventListener('focusout', () => {
-            console.log('focus out');
             if (materialInput.childNodes[3] && materialInput.childNodes[3].value.length <= 0) {
-                console.log('childNode 3 : ', materialInput.childNodes[3].value);
-                console.log('okkkk');
                 materialInput.childNodes[1].style.color = "#AAA";
                 materialInput.childNodes[1].style.fontSize = "1em";
                 materialInput.childNodes[1].style.top = "0.9em";
